9ecd2e7e00fd46b99e6de7648d5914e186de4a09
Myles Megyesi <mylesmegyesi@me.com>
2014-01-02 09:03:38 -0600

    array map: assoc when key already exists

--- src/clj/clojure/lang/persistent_array_map.clj
+++ src/clj/clojure/lang/persistent_array_map.clj
@@ -1,5 +1,5 @@
 (ns clojure.lang.persistent-array-map
-  (:refer-clojure :only [defn defn- deftype let + or loop < / inc])
+  (:refer-clojure :only [declare defn defn- deftype let + or loop < / inc when when-let if-let])
   (:require [clojure.lang.equivalence    :refer [=]]
             [clojure.lang.iassociative   :refer [IAssociative]]
             [clojure.lang.icounted       :refer [ICounted]]
@@ -8,32 +8,45 @@
 
 (defn- index-of [arr size value]
   (loop [i 0]
-    (if (< i size)
+    (when (< i size)
       (if (= value (arr/array-get arr i))
         i
-        (recur (+ i 2)))
-      -1)))
+        (recur (+ i 2))))))
 
-(deftype PersistentArrayMap [arr size count]
-  IAssociative
-  (-assoc [this k v]
+(declare make-array-map)
+
+(defn- array-map-assoc [this arr size count k v]
+  (if-let [idx (index-of arr size k)] ; key exists
+    (let [value-idx (inc idx)]
+      (if (= v (arr/array-get arr value-idx))
+        this ; key exists and value is the same, do nothing
+        (let [new-array (arr/make-array size)]
+          (arr/array-copy! arr 0 new-array 0 size)
+          (arr/array-set! new-array value-idx v)
+          (make-array-map new-array size count))))
     (let [new-size (+ size 2)
           new-array (arr/make-array new-size)]
       (arr/array-copy! arr 0 new-array 2 size)
       (arr/array-set! new-array 0 k)
       (arr/array-set! new-array 1 v)
-      (PersistentArrayMap. new-array new-size (/ new-size 2))))
+      (make-array-map new-array new-size (/ new-size 2)))))
+
+(deftype PersistentArrayMap [arr size count]
+  IAssociative
+  (-assoc [this k v]
+    (array-map-assoc this arr size count k v))
 
   ILookup
   (-lookup [this key]
-    (let [idx (index-of arr size key)]
-      (if (= -1 idx)
-        nil
-        (arr/array-get arr (inc idx)))))
+    (when-let [idx (index-of arr size key)]
+      (arr/array-get arr (inc idx))))
 
   ICounted
   (-count [this] count)
   )
+
+(defn- make-array-map [arr size count]
+  (PersistentArrayMap. arr size count))
 
 (defn array-map [& args]
   (let [[arr size] (arr/make-array-with-items (or args []))]
--- test/clj/clojure/lang/persistent_array_map_test.clj
+++ test/clj/clojure/lang/persistent_array_map_test.clj
@@ -23,4 +23,23 @@
       (is (= 0 (count m1)))
       (is (= 1 (count m2)))))
 
+  (testing "associates a key to a value when the key already exists"
+    (let [m1 (array-map)
+          m2 (assoc m1 :key 1)
+          m3 (assoc m2 :key 2)]
+      (is (not (identical? m2 m3)))
+      (is (not= m2 m3))
+      (is (nil? (get m1 :key)))
+      (is (= 1 (get m2 :key)))
+      (is (= 2 (get m3 :key)))
+      (is (= 0 (count m1)))
+      (is (= 1 (count m2)))
+      (is (= 1 (count m3)))))
+
+  (testing "associates a key to a value when the key already exists and the value is the same"
+    (let [m1 (array-map)
+          m2 (assoc m1 :key 1)
+          m3 (assoc m2 :key 1)]
+      (is (identical? m2 m3))))
+
   )
