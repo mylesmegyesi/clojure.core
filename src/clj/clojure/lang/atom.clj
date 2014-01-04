324db406dce40918f44a34ec6df49199f2cdb79a
Patrick Gombert <patrickgombert@gmail.com>
2014-01-04 09:27:53 -0600

    Use java's AtomicReference to back atoms. Add compare-and-set-entity! into the platform.

--- src/clj/clojure/lang/atom.clj
+++ src/clj/clojure/lang/atom.clj
@@ -1,9 +1,9 @@
 (ns clojure.lang.atom
-  (:refer-clojure :only [apply defn defn- deftype let into])
-  (:require [clojure.lang.iatom                   :refer [IAtom -compare-and-set! -reset! -swap!]]
-            [clojure.lang.ideref                  :refer [IDeref]]
-            [clojure.lang.equivalence             :refer [=]]
-            [clojure.lang.platform.mutable-entity :as    ent]))
+  (:refer-clojure :only [apply defn deftype let into])
+  (:require [clojure.lang.iatom                  :refer [IAtom -compare-and-set! -reset! -swap!]]
+            [clojure.lang.ideref                 :refer [IDeref]]
+            [clojure.lang.equivalence            :refer [=]]
+            [clojure.lang.platform.atomic-entity :as    ent]))
 
 (defn compare-and-set! [atm old-state new-state]
   (-compare-and-set! atm old-state new-state))
@@ -23,11 +23,7 @@
 
   IAtom
   (-compare-and-set! [this old-state new-state]
-    (if (= (ent/get-entity -state) old-state)
-      (do
-        (ent/set-entity! -state new-state)
-        true)
-      false))
+    (ent/compare-and-set-entity! -state old-state new-state))
 
   (-reset! [this new-state]
     (do
@@ -38,10 +34,10 @@
     (let [entity (ent/get-entity -state)
           arg-list (into [entity] args)
           updated-entity (apply f arg-list)]
-      (ent/set-entity! -state updated-entity)
-      updated-entity))
+      (if (ent/compare-and-set-entity! -state entity updated-entity)
+        updated-entity)))
 
   )
 
 (defn atom [state]
-  (Atom. (ent/make-mutable-entity state)))
+  (Atom. (ent/make-atomic-entity state)))
