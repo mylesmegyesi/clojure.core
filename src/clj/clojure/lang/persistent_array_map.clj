4d504380abc4753d05425a5fba3efdfc93b12def
Patrick Gombert <patrickgombert@gmail.com>
2014-01-27 20:15:30 -0600

    Implement IMeta for PersistentArrayMap and PersistentTreeMap

--- src/clj/clojure/lang/persistent_array_map.clj
+++ src/clj/clojure/lang/persistent_array_map.clj
@@ -4,6 +4,7 @@
             [clojure.lang.counted                :refer [count]]
             [clojure.lang.icounted               :refer [ICounted]]
             [clojure.lang.ilookup                :refer [ILookup]]
+            [clojure.lang.imeta                  :refer [IMeta]]
             [clojure.lang.ipersistent-map        :refer [IPersistentMap]]
             [clojure.lang.iseq                   :refer [ISeq]]
             [clojure.lang.iseqable               :refer [ISeqable]]
@@ -31,16 +32,16 @@
 (defn- array-map-contains? [arr size k]
   (not (nil? (index-of arr size k))))
 
-(defn- array-map-dissoc [this arr size count k]
+(defn- array-map-dissoc [this arr size count k meta]
   (if-let [idx (index-of arr size k)] ; key exists
     (let [new-size (- size 2)
           new-array (arr/make-array new-size)]
       (arr/array-copy! arr 0 new-array 0 idx)
       (arr/array-copy! arr (+ idx 2) new-array idx (- size idx 2))
-      (make-array-map new-array new-size (dec count)))
+      (make-array-map new-array new-size (dec count) meta))
     this))
 
-(defn- array-map-assoc [this arr size count k v]
+(defn- array-map-assoc [this arr size count k v meta]
   (if-let [idx (index-of arr size k)] ; key exists
     (let [value-idx (inc idx)]
       (if (= v (arr/array-get arr value-idx))
@@ -48,13 +49,13 @@
         (let [new-array (arr/make-array size)]
           (arr/array-copy! arr 0 new-array 0 size)
           (arr/array-set! new-array value-idx v)
-          (make-array-map new-array size count))))
+          (make-array-map new-array size count meta))))
     (let [new-size (+ size 2)
           new-array (arr/make-array new-size)]
       (arr/array-copy! arr 0 new-array 2 size)
       (arr/array-set! new-array 0 k)
       (arr/array-set! new-array 1 v)
-      (make-array-map new-array new-size (/ new-size 2)))))
+      (make-array-map new-array new-size (/ new-size 2) meta))))
 
 (defn- array-map-lookup [arr size key not-found]
   (if-let [idx (index-of arr size key)]
@@ -103,24 +104,32 @@
 
 (defmacro defpersistentarraymap [type]
   (list*
-    'deftype type ['-arr '-size '-count '-seq]
+    'deftype type ['-arr '-size '-count '-seq '-meta]
 
     'ICounted
     (list '-count ['this] '-count)
 
     'ILookup
     (list '-lookup ['this 'k 'not-found]
-          (list 'array-map-lookup '-arr '-size 'k 'not-found))
+      (list 'array-map-lookup '-arr '-size 'k 'not-found))
 
     (list '-includes? ['this 'k]
-          (list 'array-map-contains? '-arr '-size 'k))
+      (list 'array-map-contains? '-arr '-size 'k))
+
+    'IMeta
+    (list '-meta ['this] '-meta)
+
+    (list '-with-meta ['this 'm]
+      (list 'let ['new-arr (list 'arr/make-array '-size)]
+        (list 'arr/array-copy! '-arr 0 'new-arr 0 '-size)
+        (list 'make-array-map 'new-arr '-size '-count 'm)))
 
     'IPersistentMap
     (list '-assoc ['this 'k 'v]
-          (list 'array-map-assoc 'this '-arr '-size '-count 'k 'v))
+      (list 'array-map-assoc 'this '-arr '-size '-count 'k 'v '-meta))
 
     (list '-dissoc ['this 'k]
-          (list 'array-map-dissoc 'this '-arr '-size '-count 'k))
+      (list 'array-map-dissoc 'this '-arr '-size '-count 'k '-meta))
 
     'ISeqable
     (list '-seq ['this] '-seq)
@@ -129,13 +138,13 @@
 
 (defpersistentarraymap PersistentArrayMap)
 
-(defn- make-array-map [arr size count]
-  (PersistentArrayMap. arr size count (make-array-map-seq arr count (dec count) 0)))
+(defn- make-array-map [arr size count meta]
+  (PersistentArrayMap. arr size count (make-array-map-seq arr count (dec count) 0) meta))
 
 (defn array-map [& args]
   (let [[arr size] (arr/make-array-with-items (or args []))]
     (if (even? size)
-      (make-array-map arr size (/ size 2))
+      (make-array-map arr size (/ size 2) nil)
       (throw (new-argument-error
                (format "PersistentArrayMap can only be created with even number of arguments: %s arguments given"
                        size))))))
