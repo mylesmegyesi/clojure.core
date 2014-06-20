a79730794ad6a44085454ff86cdec81c8cb94f07
Patrick Gombert <patrickgombert@gmail.com>
2014-06-20 16:00:27 -0500

    Implement rand-int

--- src/clj/clojure/next.clj
+++ src/clj/clojure/next.clj
@@ -194,12 +194,6 @@
 (defn zero? [i]
   (is-zero? i))
 
-(require ['clojure.lang.platform.random :refer ['rand-float]])
-
-(defn rand
-  ([] (rand-float))
-  ([n] (* n (rand))))
-
 (defn count [obj]
   (-count obj))
 
@@ -259,6 +253,15 @@
 (defn pop [coll]
   (when coll
     (-pop coll)))
+
+(require ['clojure.lang.platform.random :refer ['rand-float]])
+
+(defn rand
+  ([] (rand-float))
+  ([n] (* n (rand))))
+
+(defn rand-int [n]
+  (int (rand n)))
 
 (require ['clojure.lang.aseq])
 (require ['clojure.lang.platform.seqable])
