3747b922e6bd3ea3e116de1c3a72c3e104749964
Patrick Gombert <patrickgombert@gmail.com>
2015-06-26 03:22:32 +0000

    Implement cons and empty for maps

--- test/jvm/clojure/lang/platform/persistent_map_test.clj
+++ test/jvm/clojure/lang/platform/persistent_map_test.clj
@@ -1,16 +1,21 @@
 (ns clojure.lang.platform.persistent-map-test
-  (:refer-clojure :only [defn let])
-  (:require [clojure.test :refer :all]
-            [clojure.next :refer :all])
-  (:import [java.util NoSuchElementException]))
+  (:refer-clojure :only [defn defn- let list ->])
+  (:require [clojure.test             :refer :all]
+            [clojure.support.test-seq :refer [test-seqable]]
+            [clojure.next             :refer :all])
+  (:import [java.util AbstractMap AbstractMap$SimpleEntry
+                      NoSuchElementException]))
 
-(defn platform-equals-test [constructor]
+(defn- make-map-entry [k v]
+  (AbstractMap$SimpleEntry. k v))
+
+(defn- platform-equals-test [constructor]
   (testing "one map is .equals to another"
     (let [m1 (constructor :k1 1)
           m2 (constructor :k1 1)]
       (is (.equals m1 m2)))))
 
-(defn platform-iterable-test [constructor]
+(defn- platform-iterable-test [constructor]
   (testing "hasNext when there is one item"
     (let [iterator (.iterator (constructor :k1 1))]
       (is (.hasNext iterator))))
@@ -33,12 +38,28 @@
     (let [iterator (.iterator (constructor))]
       (is (thrown? UnsupportedOperationException (.remove iterator))))))
 
-(defn platform-hash-code-test [constructor]
+(defn- platform-hash-code-test [constructor]
   (testing "returns the hashCode"
     (let [m1 (constructor :k 1)]
       (is (= (hash m1) (.hashCode m1))))))
 
+(defn- platform-conj-test [constructor]
+  (testing "conj a map entry"
+    (let [m (conj (constructor) (make-map-entry :k :v))]
+      (is (= 1 (count m)))
+      (is (= :k (key (first m))))
+      (is (= :v (val (first m))))))
+
+  (testing "conj a seq of map entries"
+    (let [sq (test-seqable (list (make-map-entry :k1 1) (make-map-entry :k2 2)))
+          m (-> (constructor) (conj sq))
+          ks (keys m)
+          vs (vals m)]
+      (is (= 2 (count m))))))
+
 (defn platform-map-test [constructor]
   (platform-equals-test constructor)
   (platform-iterable-test constructor)
-  (platform-hash-code-test constructor))
+  (platform-hash-code-test constructor)
+  (platform-conj-test constructor))
+
